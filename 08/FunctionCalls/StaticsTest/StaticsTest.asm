bfaf2cafb357f0ebefd6a3611d0375fec42e3a7d
whizen1203 <84667471+whizen1203@users.noreply.github.com>
2023-09-15 11:25:30 +0900

    Add files via upload

---
+++ 08/FunctionCalls/StaticsTest/StaticsTest.asm
@@ -0,0 +1,784 @@
+
+// bootstrap code
+@256
+D=A
+@SP
+M=D
+
+// call Sys.init 0
+// push return-address
+@Sys.init$ret.1
+D=A
+@SP
+A=M
+M=D
+@SP
+M=M+1
+// push LCL
+@LCL
+D=M
+@SP
+A=M
+M=D
+@SP
+M=M+1
+// push ARG
+@ARG
+D=M
+@SP
+A=M
+M=D
+@SP
+M=M+1
+// push THIS
+@THIS
+D=M
+@SP
+A=M
+M=D
+@SP
+M=M+1
+// push THAT
+@THAT
+D=M
+@SP
+A=M
+M=D
+@SP
+M=M+1
+// ARG = SP-n-5
+@SP
+D=M
+@0
+D=D-A
+@5
+D=D-A
+@ARG
+M=D
+// LCL = SP
+@SP
+D=M
+@LCL
+M=D
+// goto f
+@Sys.init
+0;JMP
+// (return-address)
+(Sys.init$ret.1)
+
+// function Class1.set 0
+(Class1.set)
+
+
+// push argument 0
+@ARG
+D=M
+@0
+D=D+A
+A=D
+D=M
+@SP
+A=M
+M=D
+@SP
+M=M+1
+
+// pop static 0
+@SP
+M=M-1
+A=M
+D=M
+@Class1.0
+M=D
+
+// push argument 1
+@ARG
+D=M
+@1
+D=D+A
+A=D
+D=M
+@SP
+A=M
+M=D
+@SP
+M=M+1
+
+// pop static 1
+@SP
+M=M-1
+A=M
+D=M
+@Class1.1
+M=D
+
+// push constant 0
+@0
+D=A
+@SP
+A=M
+M=D
+@SP
+M=M+1
+
+// return
+@LCL
+D=M
+@frame
+M=D // FRAME = LCL
+@5
+D=D-A
+A=D
+D=M
+@return_address
+M=D // RET = *(FRAME-5)
+@SP
+M=M-1
+A=M
+D=M
+@ARG
+A=M
+M=D // *ARG = pop()
+@ARG
+D=M+1
+@SP
+M=D // SP = ARG+1
+@frame
+D=M-1
+A=D
+D=M
+@THAT
+M=D // THAT = *(FRAME-1)
+@2
+D=A
+@frame
+D=M-D
+A=D
+D=M
+@THIS
+M=D // THIS = *(FRAME-2)
+@3
+D=A
+@frame
+D=M-D
+A=D
+D=M
+@ARG
+M=D // ARG = *(FRAME-3)
+@4
+D=A
+@frame
+D=M-D
+A=D
+D=M
+@LCL
+M=D // LCL = *(FRAME-4)
+@return_address
+A=M
+0;JMP // goto RET
+
+// function Class1.get 0
+(Class1.get)
+
+
+// push static 0
+@Class1.0
+D=M
+@SP
+A=M
+M=D
+@SP
+M=M+1
+
+// push static 1
+@Class1.1
+D=M
+@SP
+A=M
+M=D
+@SP
+M=M+1
+
+// sub
+@SP
+M=M-1
+A=M
+D=M
+A=A-1
+D=M-D
+M=D
+
+// return
+@LCL
+D=M
+@frame
+M=D // FRAME = LCL
+@5
+D=D-A
+A=D
+D=M
+@return_address
+M=D // RET = *(FRAME-5)
+@SP
+M=M-1
+A=M
+D=M
+@ARG
+A=M
+M=D // *ARG = pop()
+@ARG
+D=M+1
+@SP
+M=D // SP = ARG+1
+@frame
+D=M-1
+A=D
+D=M
+@THAT
+M=D // THAT = *(FRAME-1)
+@2
+D=A
+@frame
+D=M-D
+A=D
+D=M
+@THIS
+M=D // THIS = *(FRAME-2)
+@3
+D=A
+@frame
+D=M-D
+A=D
+D=M
+@ARG
+M=D // ARG = *(FRAME-3)
+@4
+D=A
+@frame
+D=M-D
+A=D
+D=M
+@LCL
+M=D // LCL = *(FRAME-4)
+@return_address
+A=M
+0;JMP // goto RET
+
+// function Sys.init 0
+(Sys.init)
+
+
+// push constant 6
+@6
+D=A
+@SP
+A=M
+M=D
+@SP
+M=M+1
+
+// push constant 8
+@8
+D=A
+@SP
+A=M
+M=D
+@SP
+M=M+1
+
+// call Class1.set 2
+// push return-address
+@Class1.set$ret.4
+D=A
+@SP
+A=M
+M=D
+@SP
+M=M+1
+// push LCL
+@LCL
+D=M
+@SP
+A=M
+M=D
+@SP
+M=M+1
+// push ARG
+@ARG
+D=M
+@SP
+A=M
+M=D
+@SP
+M=M+1
+// push THIS
+@THIS
+D=M
+@SP
+A=M
+M=D
+@SP
+M=M+1
+// push THAT
+@THAT
+D=M
+@SP
+A=M
+M=D
+@SP
+M=M+1
+// ARG = SP-n-5
+@SP
+D=M
+@2
+D=D-A
+@5
+D=D-A
+@ARG
+M=D
+// LCL = SP
+@SP
+D=M
+@LCL
+M=D
+// goto f
+@Class1.set
+0;JMP
+// (return-address)
+(Class1.set$ret.4)
+
+// pop temp 0
+@5
+D=A
+@0
+D=D+A
+@addr_5
+M=D
+@SP
+M=M-1
+A=M
+D=M
+@addr_5
+A=M
+M=D
+
+// push constant 23
+@23
+D=A
+@SP
+A=M
+M=D
+@SP
+M=M+1
+
+// push constant 15
+@15
+D=A
+@SP
+A=M
+M=D
+@SP
+M=M+1
+
+// call Class2.set 2
+// push return-address
+@Class2.set$ret.8
+D=A
+@SP
+A=M
+M=D
+@SP
+M=M+1
+// push LCL
+@LCL
+D=M
+@SP
+A=M
+M=D
+@SP
+M=M+1
+// push ARG
+@ARG
+D=M
+@SP
+A=M
+M=D
+@SP
+M=M+1
+// push THIS
+@THIS
+D=M
+@SP
+A=M
+M=D
+@SP
+M=M+1
+// push THAT
+@THAT
+D=M
+@SP
+A=M
+M=D
+@SP
+M=M+1
+// ARG = SP-n-5
+@SP
+D=M
+@2
+D=D-A
+@5
+D=D-A
+@ARG
+M=D
+// LCL = SP
+@SP
+D=M
+@LCL
+M=D
+// goto f
+@Class2.set
+0;JMP
+// (return-address)
+(Class2.set$ret.8)
+
+// pop temp 0
+@5
+D=A
+@0
+D=D+A
+@addr_9
+M=D
+@SP
+M=M-1
+A=M
+D=M
+@addr_9
+A=M
+M=D
+
+// call Class1.get 0
+// push return-address
+@Class1.get$ret.10
+D=A
+@SP
+A=M
+M=D
+@SP
+M=M+1
+// push LCL
+@LCL
+D=M
+@SP
+A=M
+M=D
+@SP
+M=M+1
+// push ARG
+@ARG
+D=M
+@SP
+A=M
+M=D
+@SP
+M=M+1
+// push THIS
+@THIS
+D=M
+@SP
+A=M
+M=D
+@SP
+M=M+1
+// push THAT
+@THAT
+D=M
+@SP
+A=M
+M=D
+@SP
+M=M+1
+// ARG = SP-n-5
+@SP
+D=M
+@0
+D=D-A
+@5
+D=D-A
+@ARG
+M=D
+// LCL = SP
+@SP
+D=M
+@LCL
+M=D
+// goto f
+@Class1.get
+0;JMP
+// (return-address)
+(Class1.get$ret.10)
+
+// call Class2.get 0
+// push return-address
+@Class2.get$ret.11
+D=A
+@SP
+A=M
+M=D
+@SP
+M=M+1
+// push LCL
+@LCL
+D=M
+@SP
+A=M
+M=D
+@SP
+M=M+1
+// push ARG
+@ARG
+D=M
+@SP
+A=M
+M=D
+@SP
+M=M+1
+// push THIS
+@THIS
+D=M
+@SP
+A=M
+M=D
+@SP
+M=M+1
+// push THAT
+@THAT
+D=M
+@SP
+A=M
+M=D
+@SP
+M=M+1
+// ARG = SP-n-5
+@SP
+D=M
+@0
+D=D-A
+@5
+D=D-A
+@ARG
+M=D
+// LCL = SP
+@SP
+D=M
+@LCL
+M=D
+// goto f
+@Class2.get
+0;JMP
+// (return-address)
+(Class2.get$ret.11)
+
+// label WHILE
+(WHILE)
+
+// goto WHILE
+@WHILE
+0;JMP
+
+// function Class2.set 0
+(Class2.set)
+
+
+// push argument 0
+@ARG
+D=M
+@0
+D=D+A
+A=D
+D=M
+@SP
+A=M
+M=D
+@SP
+M=M+1
+
+// pop static 0
+@SP
+M=M-1
+A=M
+D=M
+@Class2.0
+M=D
+
+// push argument 1
+@ARG
+D=M
+@1
+D=D+A
+A=D
+D=M
+@SP
+A=M
+M=D
+@SP
+M=M+1
+
+// pop static 1
+@SP
+M=M-1
+A=M
+D=M
+@Class2.1
+M=D
+
+// push constant 0
+@0
+D=A
+@SP
+A=M
+M=D
+@SP
+M=M+1
+
+// return
+@LCL
+D=M
+@frame
+M=D // FRAME = LCL
+@5
+D=D-A
+A=D
+D=M
+@return_address
+M=D // RET = *(FRAME-5)
+@SP
+M=M-1
+A=M
+D=M
+@ARG
+A=M
+M=D // *ARG = pop()
+@ARG
+D=M+1
+@SP
+M=D // SP = ARG+1
+@frame
+D=M-1
+A=D
+D=M
+@THAT
+M=D // THAT = *(FRAME-1)
+@2
+D=A
+@frame
+D=M-D
+A=D
+D=M
+@THIS
+M=D // THIS = *(FRAME-2)
+@3
+D=A
+@frame
+D=M-D
+A=D
+D=M
+@ARG
+M=D // ARG = *(FRAME-3)
+@4
+D=A
+@frame
+D=M-D
+A=D
+D=M
+@LCL
+M=D // LCL = *(FRAME-4)
+@return_address
+A=M
+0;JMP // goto RET
+
+// function Class2.get 0
+(Class2.get)
+
+
+// push static 0
+@Class2.0
+D=M
+@SP
+A=M
+M=D
+@SP
+M=M+1
+
+// push static 1
+@Class2.1
+D=M
+@SP
+A=M
+M=D
+@SP
+M=M+1
+
+// sub
+@SP
+M=M-1
+A=M
+D=M
+A=A-1
+D=M-D
+M=D
+
+// return
+@LCL
+D=M
+@frame
+M=D // FRAME = LCL
+@5
+D=D-A
+A=D
+D=M
+@return_address
+M=D // RET = *(FRAME-5)
+@SP
+M=M-1
+A=M
+D=M
+@ARG
+A=M
+M=D // *ARG = pop()
+@ARG
+D=M+1
+@SP
+M=D // SP = ARG+1
+@frame
+D=M-1
+A=D
+D=M
+@THAT
+M=D // THAT = *(FRAME-1)
+@2
+D=A
+@frame
+D=M-D
+A=D
+D=M
+@THIS
+M=D // THIS = *(FRAME-2)
+@3
+D=A
+@frame
+D=M-D
+A=D
+D=M
+@ARG
+M=D // ARG = *(FRAME-3)
+@4
+D=A
+@frame
+D=M-D
+A=D
+D=M
+@LCL
+M=D // LCL = *(FRAME-4)
+@return_address
+A=M
+0;JMP // goto RET
